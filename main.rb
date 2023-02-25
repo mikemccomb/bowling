ff7228b6a16591788714706886dc49d806e6df90
mikemccomb <mikemccomb@gmail.com>
2023-02-25 10:31:12 -0600

    Removed junked code

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require "tty-prompt"
 
 prompt = TTY::Prompt.new
-# scorecard = [["-", "-"], [" ", " "], [" ", " "], [" ", " "], [" ", " "], [" ", " "], [" ", " "], [" ", " "], [" ", " "], [" ", " ", " "]]
 score_arr = []
 score = 0
 
@@ -23,9 +22,7 @@ if roll == 10
   on_strike += 1
   frame1 = 10
   score_arr[0] = frame1
-  # scorecard[0][1] = "X"
 else
-  # scorecard[0][0] = roll
   roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
   frame1 = roll + roll2
   if frame1 == 10
@@ -37,7 +34,6 @@ puts "SCORE: #{score(score_arr)}"
 p score_arr
 p on_strike
 p on_spare
-# puts scorecard
 
 puts "FRAME 2"
 roll = prompt.ask("How many pins did you knock down on your first ball?").to_i
@@ -102,10 +98,7 @@ if roll == 10 # STRIKE
 else # Roll < 10
   if on_strike == 2
     score_arr[0] += roll # Close out first X
-    # score_arr[1] += roll # Apply to second X
     on_strike -= 1
-    # elsif on_strike == 1
-    #   score_arr[1] += roll # Apply to X
   elsif on_spare # Close out spare math
     score_arr[1] += roll
     on_spare = false
@@ -122,6 +115,55 @@ else # Roll < 10
   else # Spare or open frame
     score_arr[2] = frame3
     if frame3 == 10 # Spare
+      on_spare = true
+    end
+  end
+end
+puts "SCORE: #{score(score_arr)}"
+p score_arr
+puts "STRIKE: #{on_strike}"
+puts "SPARE: #{on_spare}"
+
+puts "FRAME 4"
+roll = prompt.ask("How many pins did you knock down on your first ball?").to_i
+if roll == 10 # STRIKE
+  if on_strike == 2 # Close out first X math
+    score_arr[1] += roll
+    score_arr[2] += roll
+    score_arr[3] = roll
+  elsif on_strike == 1 # Double
+    score_arr[2] += roll
+    score_arr[3] = roll
+    on_strike += 1
+  elsif on_spare # Close out spare math
+    score_arr[2] += roll
+    score_arr[3] = roll
+    on_strike += 1
+    on_spare = false
+  else
+    score_arr[3] = roll # First strike
+    on_strike += 1
+  end
+else # Roll < 10
+  if on_strike == 2
+    score_arr[1] += roll # Close out first X
+    on_strike -= 1
+  elsif on_spare # Close out spare math
+    score_arr[2] += roll
+    on_spare = false
+  end
+  roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
+  frame4 = roll + roll2
+  if on_strike == 1 # Finish previous X math
+    on_strike -= 1
+    score_arr[2] += frame4
+    score_arr[3] = frame4
+    if frame4 == 10 # Spare
+      on_spare = true
+    end
+  else # Spare or open frame
+    score_arr[3] = frame4
+    if frame4 == 10 # Spare
       on_spare = true
     end
   end
