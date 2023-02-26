ca31319a1631e54d8acf2724fa810bb5b8c8ca97
mikemccomb <mikemccomb@gmail.com>
2023-02-26 17:45:22 -0600

    Created print(); updated notes

--- main.rb
+++ main.rb
@@ -12,29 +12,30 @@ def score(arr)
   return score
 end
 
+def print(score_arr, on_strike, on_spare, frame)
+  system "clear"
+  puts "SCORE: #{score(score_arr)}"
+  p score_arr
+  puts "STRIKE: #{on_strike}"
+  puts "SPARE: #{on_spare}"
+  puts "FRAME #{frame}"
+end
+
 on_strike = 0
 on_spare = false
 frame = 1
 frame_score = 0
 
 while frame < 10
-  system "clear"
-  puts "SCORE: #{score(score_arr)}"
-  p score_arr
-  puts "STRIKE: #{on_strike}" # Remove after testing
-  puts "SPARE: #{on_spare}" # Remove after testing
-  puts "FRAME #{frame}" #Remove after testing
-  roll = prompt.ask("How many pins did you knock down on your first ball?").to_i
+  print(score_arr, on_strike, on_spare, frame)
+  roll = prompt.ask("FIRST BALL:").to_i
   if roll == 10
     score_arr[frame - 1] = roll # STRIKE
     if on_strike == 2
       score_arr[frame - 3] += roll # Turkey (XXX)
       score_arr[frame - 2] += roll # Add to previous frame's X
-    elsif on_strike == 1
-      score_arr[frame - 2] += roll # Add to previous frame's X
-      on_strike += 1
-    elsif on_spare
-      score_arr[frame - 2] += roll # Add to previous frame's spare
+    elsif on_strike == 1 || on_spare
+      score_arr[frame - 2] += roll # Add to previous frame's X or /
       on_strike += 1
       on_spare = false
     else
@@ -48,7 +49,7 @@ while frame < 10
       score_arr[frame - 2] += roll # Add to previous frame's spare; simplify with line 36
       on_spare = false
     end
-    roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
+    roll2 = prompt.ask("SECOND BALL:").to_i
     frame_score = roll + roll2
     if on_strike == 1 # Finish previous X math
       on_strike -= 1
@@ -67,13 +68,9 @@ while frame < 10
   frame += 1
 end
 
-system "clear"
-puts "SCORE: #{score(score_arr)}"
-p score_arr
-puts "STRIKE: #{on_strike}"
-puts "SPARE: #{on_spare}"
-puts "FRAME #{frame}" #FRAME 10
-roll = prompt.ask("How many pins did you knock down on your first ball?").to_i
+# TENTH FRAME
+print(score_arr, on_strike, on_spare, frame)
+roll = prompt.ask("FIRST BALL:").to_i
 if roll == 10 # STRIKE ON BALL 1
   score_arr[frame - 1] = roll
   if on_strike == 2 # Close out first X math
@@ -86,12 +83,12 @@ if roll == 10 # STRIKE ON BALL 1
   else
     on_strike += 1
   end
-  roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
+  roll2 = prompt.ask("SECOND BALL:").to_i
   score_arr[frame - 1] += roll2 # Adding in 10th
   if on_strike == 2
     score_arr[frame - 2] += roll2 # Close out X in 9th
   end
-  roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
+  roll3 = prompt.ask("THIRD BALL:").to_i
   score_arr[frame - 1] += roll3 # Final ball
 else # Roll < 10
   if on_strike == 2
@@ -101,7 +98,7 @@ else # Roll < 10
     score_arr[frame - 2] += roll
     on_spare = false
   end
-  roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
+  roll2 = prompt.ask("SECOND BALL:").to_i
   frame_score = roll + roll2
   score_arr[frame - 1] = frame_score
   if on_strike == 1 # Close out X in 9th
@@ -109,12 +106,9 @@ else # Roll < 10
     on_strike -= 1 # Might be able to remove; keep when testing new game
   end
   if frame_score == 10 # Spare
-    roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
+    roll3 = prompt.ask("THIRD BALL:").to_i
     score_arr[frame - 1] += roll3 # Final ball
   end
 end
 
-puts "SCORE: #{score(score_arr)}"
-p score_arr
-puts "STRIKE: #{on_strike}"
-puts "SPARE: #{on_spare}"
+print(score_arr, on_strike, on_spare, frame)
