a495cf9576f8b72a7dde278ae418371afa7f52a7
mikemccomb <mikemccomb@gmail.com>
2023-02-26 17:14:41 -0600

    DRY 10th frame logic

--- main.rb
+++ main.rb
@@ -42,10 +42,10 @@ while frame < 10
     end
   else # Roll < 10
     if on_strike == 2
-      score_arr[frame - 3] += roll # Close out first X
-      on_strike -= 1
-    elsif on_spare # Close out spare math
-      score_arr[frame - 2] += roll
+      score_arr[frame - 3] += roll # XX + roll
+      on_strike -= 1 # Set up logic for roll2
+    elsif on_spare
+      score_arr[frame - 2] += roll # Add to previous frame's spare; simplify with line 36
       on_spare = false
     end
     roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
@@ -75,29 +75,24 @@ puts "SPARE: #{on_spare}"
 puts "FRAME #{frame}" #FRAME 10
 roll = prompt.ask("How many pins did you knock down on your first ball?").to_i
 if roll == 10 # STRIKE ON BALL 1
+  score_arr[frame - 1] = roll
   if on_strike == 2 # Close out first X math
     score_arr[frame - 3] += roll
     score_arr[frame - 2] += roll
-    score_arr[frame - 1] = roll
   elsif on_strike == 1 # Double
     score_arr[frame - 2] += roll
-    score_arr[frame - 1] = roll
     on_strike += 1
   elsif on_spare # Close out spare math
     score_arr[frame - 2] += roll
-    score_arr[frame - 1] = roll
     on_strike += 1
     on_spare = false
   else
-    score_arr[frame - 1] = roll # First strike
     on_strike += 1
   end
   roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
+  score_arr[frame - 1] += roll2 # Adding in 10th
   if on_strike == 2
     score_arr[frame - 2] += roll2 # Close out X in 9th
-    score_arr[frame - 1] += roll2 # Adding in 10th
-  elsif on_strike == 1
-    score_arr[frame - 1] += roll2 # Adding in 10th
   end
   roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
   score_arr[frame - 1] += roll3 # Final ball
@@ -112,19 +107,15 @@ else # Roll < 10
   roll2 = prompt.ask("How many pins did you knock down on your second ball?").to_i
   frame_score = roll + roll2
   if on_strike == 1 # Close out X in 9th
-    on_strike -= 1
     score_arr[frame - 2] += frame_score
     score_arr[frame - 1] = frame_score
-    if frame_score == 10 # Spare
-      roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
-      score_arr[frame - 1] += roll3 # Final ball
-    end
+    on_strike -= 1
   else # Spare or open frame
     score_arr[frame - 1] = frame_score
-    if frame_score == 10 # Spare
-      roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
-      score_arr[frame - 1] += roll3 # Final ball
-    end
+  end
+  if frame_score == 10 # Spare
+    roll3 = prompt.ask("How many pins did you knock down on your third ball?").to_i
+    score_arr[frame - 1] += roll3 # Final ball
   end
 end
 
