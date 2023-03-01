abbbfccf896097b8d70ebfea61bd79e5395b7b6b
mikemccomb <mikemccomb@gmail.com>
2023-03-01 15:37:16 -0600

    Created roll(first) method

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require "tty-prompt"
+
 class Game
   attr_accessor :score_arr, :score, :on_strike, :on_spare, :frame, :ball
 
@@ -26,35 +28,35 @@ class Game
     puts "SPARE: #{@on_spare}"
   end
 
-  # def roll(ball, first)
-  #   prompt = TTY::Prompt.new
-  #   ask = true
-  #   while ask
-  #     roll = prompt.ask("BALL #{ball}:")
-  #     # Alts to player entering 0
-  #     if roll.upcase == "F" || roll == "-"
-  #       return roll = 0
-  #     end
-  #     # Alt to player entering 10 for a strike
-  #     if roll.upcase == "X"
-  #       puts "Nice strike!"
-  #       return roll = 10
-  #     end
-  #     # Alts for player entering a spare
-  #     if ball == 2 && ((roll.to_i + first == 10) || roll == "/")
-  #       puts "Nice spare!"
-  #       return roll = 10 - first
-  #     end
-  #     # Player enters an incorrect value
-  #     if roll.to_i > 10 || (ball == 1 && roll == "/") || (ball == 2 && (roll.to_i + first > 10)) || (ball == 3 && ((roll.to_i + first) > 30))
-  #       puts "Error. Please re-enter score."
-  #     else
-  #       ask = false
-  #     end
-  #   end
+  def roll(first)
+    prompt = TTY::Prompt.new
+    ask = true
+    while ask
+      roll = prompt.ask("BALL #{@ball}:")
+      # Alts to player entering 0
+      if roll.upcase == "F" || roll == "-"
+        return 0
+      end
+      # Alt to player entering 10 for a strike
+      if roll.upcase == "X"
+        puts "Nice strike!"
+        return 10
+      end
+      # Alts for player entering a spare
+      if @ball == 2 && ((roll.to_i + first == 10) || roll == "/")
+        puts "Nice spare!"
+        return 10 - first
+      end
+      # Player enters an incorrect value
+      if roll.to_i > 10 || (@ball == 1 && roll == "/") || (@ball == 2 && (roll.to_i + first > 10)) || (@ball == 3 && ((roll.to_i + first) > 30))
+        puts "Error. Please re-enter score."
+      else
+        ask = false
+      end
+    end
 
-  #   return roll = roll.to_i
-  # end
+    return roll.to_i
+  end
 end
 
 # test = Game.new
--- main.rb
+++ main.rb
@@ -17,37 +17,37 @@ game = Game.new
 #   return score
 # end
 
-def roll(ball, first)
-  prompt = TTY::Prompt.new
-  ask = true
-  while ask
-    roll = prompt.ask("BALL #{ball}:")
-    # Alts to player entering 0
-    if roll.upcase == "F" || roll == "-"
-      return roll = 0
-    end
-    # Alt to player entering 10 for a strike
-    if roll.upcase == "X"
-      puts "Nice strike!"
-      return roll = 10
-    end
-    # Alts for player entering a spare
-    if ball == 2 && ((roll.to_i + first == 10) || roll == "/")
-      puts "Nice spare!"
-      return roll = 10 - first
-    end
-    # Player enters an incorrect value
-    if roll.to_i > 10 || (ball == 1 && roll == "/") || (ball == 2 && (roll.to_i + first > 10)) || (ball == 3 && ((roll.to_i + first) > 30))
-      puts "Error. Please re-enter score."
-    else
-      ask = false
-    end
-  end
+# def roll(ball, first)
+#   prompt = TTY::Prompt.new
+#   ask = true
+#   while ask
+#     roll = prompt.ask("BALL #{ball}:")
+#     # Alts to player entering 0
+#     if roll.upcase == "F" || roll == "-"
+#       return roll = 0
+#     end
+#     # Alt to player entering 10 for a strike
+#     if roll.upcase == "X"
+#       puts "Nice strike!"
+#       return roll = 10
+#     end
+#     # Alts for player entering a spare
+#     if ball == 2 && ((roll.to_i + first == 10) || roll == "/")
+#       puts "Nice spare!"
+#       return roll = 10 - first
+#     end
+#     # Player enters an incorrect value
+#     if roll.to_i > 10 || (ball == 1 && roll == "/") || (ball == 2 && (roll.to_i + first > 10)) || (ball == 3 && ((roll.to_i + first) > 30))
+#       puts "Error. Please re-enter score."
+#     else
+#       ask = false
+#     end
+#   end
 
-  return roll = roll.to_i
-end
+#   return roll = roll.to_i
+# end
 
-# def print(score_arr, on_strike, on_spare, frame)
+# def game.print
 #   system "clear"
 #   puts "SCORE: #{score(score_arr)}"
 #   p score_arr
@@ -58,9 +58,9 @@ end
 
 10.times do
   game.print
-  # print(score_arr, on_strike, on_spare, frame)
+  # game.print
   # FIRST BALL
-  roll = roll(ball, 0)
+  roll = game.roll(0)
   score_arr[frame - 1] = roll
   if on_strike == 2
     score_arr[frame - 3] += roll
@@ -80,10 +80,10 @@ end
   if roll < 10 || frame == 10
     ball = 2
   end
-  print(score_arr, on_strike, on_spare, frame)
+  game.print
   # SECOND BALL
   if ball == 2
-    roll = roll(ball, score_arr[frame - 1])
+    roll = game.roll(score_arr[frame - 1])
     score_arr[frame - 1] += roll
     if on_strike == 2
       score_arr[frame - 2] += roll
@@ -104,11 +104,11 @@ end
 
   if frame == 10 && (on_spare || on_strike > 0)
     ball = 3
-    print(score_arr, on_strike, on_spare, frame)
-    roll = roll(ball, score_arr[frame - 1])
+    game.print
+    roll = game.roll(score_arr[frame - 1])
     score_arr[frame - 1] += roll # Final ball
   end
   frame += 1
 end
 
-print(score_arr, on_strike, on_spare, frame)
+game.print
