43613ecdd043369021f3c4ea9d748be210e40ced
mikemccomb <mikemccomb@gmail.com>
2023-03-01 15:12:57 -0600

    Create initialize method - tested

--- game.rb
+++ game.rb
@@ -1,2 +1,17 @@
 class Game
+  def initialize
+    @score_arr = []
+    @score = 0
+    @on_strike = 0
+    @on_spare = false
+    @frame = 1
+    @ball = 1
+  end
+
+  def score
+    return @score
+  end
 end
+
+test = Game.new
+p test.score
