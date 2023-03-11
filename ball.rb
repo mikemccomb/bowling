6468a4fc9f3c6427db8a035950537dab05641ae2
mikemccomb <mikemccomb@gmail.com>
2023-03-11 11:27:32 -0600

    Updates third_ball

--- ball.rb
+++ ball.rb
@@ -4,12 +4,6 @@
 # end
 
 def update_mark(roll) # Need to reset to 0 on spare/open
-  # 10 down on ball 1 = X
-  # 10 down by ball 2 = /
-  # If X; on_strike = 1, roll applies to previous frame (1-9)
-  # If XX; on_strike = 2, roll applies to previous 2 frames (1-9)
-  # on_strike > 0 && on_spare = true DNE
-  # If /; on_spare = true, score applies to previous frame
   if @frame == 10
     if @ball == 1
       if roll == 10
@@ -19,12 +13,11 @@ def update_mark(roll) # Need to reset to 0 on spare/open
         elsif @on_strike == 1
           # Nothing changes; ball 2 applies to 9th
         elsif @on_spare
-          @on_strike = 1 # Needed to trigger ball 3
           @on_spare = false
-        end
+        end #No new mark needed for 10th; array value used to determine B3
       else # roll < 10
         if @on_strike == 2
-          @on_strike -= 1
+          @on_strike = 1
         elsif @on_strike == 1
           # Nothing changes; ball 2 still applies to 9th
         elsif @on_spare
@@ -33,14 +26,14 @@ def update_mark(roll) # Need to reset to 0 on spare/open
       end
     elsif @ball == 2
       if @on_strike == 2
-        # DNE; B1 reduces to S1
+        # DNE; B1 reduced to S1
       elsif @on_strike == 1
         @on_strike -= 1
         # Reduce to 0 so ball three does not count toward 9th
       elsif @on_spare
         # DNE
       end
-    elsif @ball == 3
+    elsif @ball == 3 # Mark not needed; roll applies to 10F only
       if @on_strike == 2
         # DNE
       elsif @on_strike == 1
@@ -49,53 +42,37 @@ def update_mark(roll) # Need to reset to 0 on spare/open
         # DNE
       end
     end
-  else
+  else # Frames 1-9
     if @ball == 1
-      if @on_strike == 2
+      if @on_strike == 2 && (roll < 10)
+        @on_strike = 1
       elsif @on_strike == 1
+        # If strike, now XX; else B2 would still apply to prev F
+        (roll == 10) ? (@on_strike = 2) : (@on_strike = 1)
       elsif @on_spare
+        @on_spare = false
+        if roll == 10
+          @on_strike = 1
+        end
+      elsif roll == 10
+        @on_strike = 1
       end
     elsif @ball == 2
       if @on_strike == 2
+        # DNE; if B1 < 10; S = 1
       elsif @on_strike == 1
+        # Next frame would be S0
+        @on_strike = 0
+        # if frame closed, make on_spare true
+        if @score_arr[-1] == 10
+          @on_spare = true
+        end
       elsif @on_spare
+        # DNE; on_spare made false in B1
       end
-    elsif @ball == 3
+    elsif @ball == 3 # Not needed; B3 only in 10th
       if @on_strike == 2
       elsif @on_strike == 1
-      elsif @on_spare
-      end
-    end
-  else
-  if @ball == 1
-    if @strike == 2
-    elsif @strike == 1
-    elsif @on_spare
-    end
-  elsif @ball == 2
-    if @strike == 2
-    elsif @strike == 1
-    elsif @on_spare
-    end
-  elsif @ball == 3
-    if @strike == 2
-    elsif @strike == 1
-    elsif @on_spare
-    end
-  else
-    if @ball == 1
-      if @strike == 2
-      elsif @strike == 1
-      elsif @on_spare
-      end
-    elsif @ball == 2
-      if @strike == 2
-      elsif @strike == 1
-      elsif @on_spare
-      end
-    elsif @ball == 3
-      if @strike == 2
-      elsif @strike == 1
       elsif @on_spare
       end
     end
